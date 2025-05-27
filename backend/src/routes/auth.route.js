a29677f89867fd9442349890da6ddccfeeb02594
thurunuym <marasinghe.ty@gmail.com>
2025-05-28 00:49:13 +0530

    logout and login backend

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,19 +1,16 @@
 import express from "express";
 
 const router = express.Router();
-import { signup } from '../controllers/auth.controller.js';
+import { signup , login,logout} from '../controllers/auth.controller.js';
 
 
 router.post('/signup', signup);
 
 
-router.post("/login", (req,res) => {
-    res.send("login route");
-});
+router.post("/login", login);
 
-router.get("/logout", (req,res) => {
-    res.send("logout route");
-});
+router.post("/logout", logout);
 
+router.put("/update-profile",protectRoute,updateProfile);
 
 export default router;
