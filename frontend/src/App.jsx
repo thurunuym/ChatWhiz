33c5af6952dd1102f0831dad18222ed9e7cefa13
thurunuym <marasinghe.ty@gmail.com>
2025-05-31 12:21:07 +0530

    routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,49 @@
 import Navbar from './components/NavBar'; 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SigUupPage';
+import SignupPage from './pages/SignUpPage';
 import ProfilePage from './pages/ProfilePage';
-import SettingsPage from './pages/SettingsPage';
+import SettingsPage from './pages/Settings';
+import { Routes, Route ,Navigate} from 'react-router-dom';
+import { useAuthStore } from './store/useAuthStore';
+import { useEffect } from 'react';
+import {Loader} from 'lucide-react';
 
 
-function App() {
-  
+const App = () => {
+
+  const {authUser,checkAuth,isCheckingAuth} = useAuthStore();
+//This line uses JavaScript object destructuring to extract three 
+// values—authUser, checkAuth, and isCheckingAuth—from the object 
+// returned by the useAuthStore() hook.
+ 
+useEffect(() => {
+    checkAuth();
+  } , [checkAuth]
+
+  );
+
+  //This useEffect makes your app check if the user is authenticated as soon as the app loads.
+
+  console.log({authUser});
+
+  if(isCheckingAuth && !authUser) return (
+
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className="size-10 animate-spin"/>
+    </div>
+
+  )
 
   return (
     <>
       <Navbar/>
 
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/signup" element={<SignupPage/>} />
-        <Route path="/profile" element={<ProfilePage/>} />
+        <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
+        <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/"/>} />
+        <Route path="/signup" element={!authUser ? <SignupPage/> : <Navigate to="/"/>} />
+        <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login"/>} />
         <Route path="/settings" element={<SettingsPage/>} />
       </Routes>
     </>
