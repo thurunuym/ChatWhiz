aa97654411cd147293e49c052d57ffd8c2fbae41
thurunuym <marasinghe.ty@gmail.com>
2025-06-01 12:13:37 +0530

    create account ui completed

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -48,9 +48,9 @@ const SignUpPage = () => {
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <User className="size-5 text-base-content/40" />
+                  <User className="size-5 text-base-content/40 z-1"/>
                 </div>
-              </div>
+              
 
               <input
                 type="text"
@@ -63,6 +63,7 @@ const SignUpPage = () => {
                 required
               />
             </div>
+            </div>
 
             <div className="form-control">
               <label className="label">
@@ -70,9 +71,9 @@ const SignUpPage = () => {
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <Mail className="size-5 text-base-content/40" />
+                  <Mail className="size-5 text-base-content/40 z-1" />
                 </div>
-              </div>
+              
 
               <input
                 type="email"
@@ -85,6 +86,7 @@ const SignUpPage = () => {
                 required
               />
             </div>
+            </div>
 
             <div className="form-control">
               <label className="label">
@@ -92,7 +94,7 @@ const SignUpPage = () => {
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <Lock className="size-5 text-base-content/40" />
+                  <Lock className="size-5 text-base-content/40 z-1" />
                 </div>
 
                 <input
