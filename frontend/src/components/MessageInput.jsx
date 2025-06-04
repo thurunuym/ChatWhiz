e658e024391cb37422e08faa2abbe89c6304285f
thurunuym <marasinghe.ty@gmail.com>
2025-06-04 11:10:16 +0530

    messageInput bug fixed

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { useState } from 'react';
+import {useRef , useState}  from 'react'
 import { useChatStore } from '../store/useChatStore';
 import { X } from 'lucide-react';
 import { Image } from 'lucide-react';
 
 
 const MessageInput = () => {
-  const [text , setText] = React.useState('');
-  const [imagePreview, setImagePreview] = React.useState(null);
-  const fileInputRef = useState(null);
+  const [text , setText] = useState('');
+  const [imagePreview, setImagePreview] = useState(null);
+  const fileInputRef = useRef(null);
   const {sendMessage, selectedUser} = useChatStore();
 
   const handleImageChange = (e) => {};
@@ -20,27 +19,32 @@ const MessageInput = () => {
   return(
     <div className='p-4 w-full'>
       {imagePreview && (
+        
         <div className='mb-3 flex items-center gap-2'>
+          <div className='relative'>
           <img src={imagePreview} alt="Preview" 
           className='w-20 h-20 object-cover rounded-lg border border-zinc-700 '/>
           <button
           onClick={removeImage}
-          className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300 flex-items-center justify-center"
+          className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300 flex items-center justify-center"
           type="button"
           >
           <X className="size-3"/>
           </button>
         </div>
+        </div>
       )}
 
-      <form onSubmit={handleSendMessage} className='flex-items-center gap-2'>
-        <div className='flex-1 flex-gap'>
+      <form onSubmit={handleSendMessage} className='flex items-center gap-2'>
+        <div className='flex-1 flex gap-2'>
+
         <input type="text"
         className='w-full input input-bordered rounded-lg input-sm sm:input-md'
         placeholder='Type a message...'
         value={text}
         onChange={(e) => setText(e.target.value)}
         />
+
         <input type="file"
         accept='image/*'
         className='hidden'
@@ -51,7 +55,7 @@ const MessageInput = () => {
         type='button'
         className={`hidden sm:flex btn btn-circle
         ${imagePreview ? "text-emerald-500" : "text-zinc-400"}`}
-        onClick={()=> fileInputRef}
+        onClick={()=> fileInputRef.current.click()}
         >
           <Image size={20} />
         </button>
