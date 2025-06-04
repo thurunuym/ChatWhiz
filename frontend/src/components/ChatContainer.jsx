227f1552440e5dd1640f63700120df36fd7ffc79
thurunuym <marasinghe.ty@gmail.com>
2025-06-04 10:14:06 +0530

    messageInput function

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useChatStore } from '../store/useChatStore';
 import ChatHeader from './ChatHeader';
 import MessageInput from './MessageInput';
+import MessageSkeleton from './skeletons/MessageSkeleton';
 
 const ChatContainer = () => {
 
@@ -12,7 +13,13 @@ useEffect(() => {
   getMessages(selectedUser?._id);
 },[selectedUser._id, getMessages]);
 
-if(isMessagesLoading) return <div>Loading...</div>;
+if(isMessagesLoading) return (
+  <div className='flex-1 flex flex-col overflow-auto'>
+    <ChatHeader/>
+    <MessageSkeleton/>
+    <MessageInput/>
+  </div>
+)
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
