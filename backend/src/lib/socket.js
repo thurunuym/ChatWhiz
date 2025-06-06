6c271130df87d9a3d0915eee64d8992d12b448d1
thurunuym <marasinghe.ty@gmail.com>
2025-06-06 18:30:56 +0530

    socket.js configured

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -11,4 +11,12 @@ const io = new Server(server , {
     },
 });
 
+io.on("connection" , (socket) => {
+    console.log("A user connected" , socket.id);
+
+    socket.on("disconnect" , () => {
+        console.log("A user disconnected" , socket.id);
+    });
+});
+
 export {io, app, server};
