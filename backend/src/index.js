3ff259fd34891d03de9cd0a3ce18eaa58d7878cf
thurunuym <marasinghe.ty@gmail.com>
2025-06-06 18:30:41 +0530

    socket.js configured

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ import {connectDB} from './lib/db.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import {app , server} from "./lib/socket.js";
 
 dotenv.config();
-
-const app = express();
+ 
+//const app = express();    
+// (already implemented in socket.js)
 //brings in the Express library, which is a toolkit for building web servers and APIs in Node.js.
 
 const PORT = process.env.PORT 
@@ -40,7 +42,7 @@ app.use('/api/auth', authRoutes);
 
 app.use('/api/messages', messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
     connectDB(); 
     //This connects to the MongoDB database using the connection string defined in your .env file.	
