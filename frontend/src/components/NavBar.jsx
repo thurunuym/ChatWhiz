7cd1a640cee72ee20314d497ca801f95d3f9e8e2
thurunuym <marasinghe.ty@gmail.com>
2025-06-07 00:28:00 +0530

    Logo added

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
 import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import logo from "../../public/logo_chatwhiz.png";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
@@ -14,10 +15,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
-              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                <MessageSquare className="w-5 h-5 text-primary" />
-              </div>
-              <h1 className="text-lg font-bold">Chatty</h1>
+              <img src={logo} alt="ChatWhiz Logo" className="h-16 w-auto" />
             </Link>
           </div>
 
