0854adcf6ce7133ede4b0d313669447660c0f8f2
thurunuym <marasinghe.ty@gmail.com>
2025-06-10 18:37:53 +0530

    commit

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
   if (isUsersLoading) return <SidebarSkeleton />;
 
   return (
-    <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
+    <aside className="h-full w-20 lg:min-w-72 border-r border-base-300 flex flex-col transition-all duration-200">
       <div className="border-b border-base-300 w-full p-5">
         <div className="flex items-center gap-2">
           <Users className="size-6" />
